87efc77903262c4bde53fbc0abd1fc6e1417550c
Erich Welz <erichwelz@gmail.com>
2013-10-23 16:01:11 -0400

    Ex.1 to Instructor Greeting

--- people.rb
+++ people.rb
@@ -1,5 +1,30 @@
-class Studend
+class Person
+      attr_accessor :name
+    def greeting()
+      puts "Hi, my name is #{@name}."
+    end
 end
 
-Class Instructor
+class Student < Person
+  def learn()
+    puts "I get it"
+  end
 end
+
+class Instructor < Person
+  def teach()
+    puts "Everything in Ruby is an Object"
+  end
+end
+
+instructor = Instructor.new
+instructor.name = "Chris"
+instructor.greeting #Hi, my name is name
+
+
+# person.name.greeting
+
+# puts Student.new.learn
+# Chris = instructor.new
+# Chris.name = "Chris"
+# puts Chris.greeting
